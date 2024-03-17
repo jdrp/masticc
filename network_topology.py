47cea0e797757a81c3c2620a153e873fded9baec
jdrp <jdiazderabago@gmail.com>
2024-03-17 14:05:52 +0100

    basic network topology

---
+++ network_topology.py
@@ -0,0 +1,62 @@
+try:
+    from ns import ns
+except ModuleNotFoundError:
+    raise SystemExit(
+        "Error: ns3 Python module not found;"
+        " Python bindings may not be enabled"
+        " or your PYTHONPATH might not be properly configured"
+    )
+import sys
+from ctypes import c_bool, c_int
+
+# command line arguments
+nClients = c_int(3)
+accessRate = c_int(1024)  # Mbps
+accessDelay = c_int(5)  # ms
+bottleneckRate = c_int(5)  # Mbps
+bottleneckDelay = c_int(10)  # ms
+verbose = c_bool(True)
+cmd = ns.CommandLine(__file__)
+cmd.AddValue("nClients", "Number of clientes connected to R1", nClients)
+cmd.AddValue("accessRate", "Rate of access links (Mbps)", accessRate)
+cmd.AddValue("accessDelay", "Delay of access links (ms)", accessDelay)
+cmd.AddValue("bottleneckRate", "Rate of access links (Mbps)", bottleneckRate)
+cmd.AddValue("bottleneckDelay", "Delay of access links (ms)", bottleneckDelay)
+cmd.AddValue("verbose", "Tell echo applications to log if true", verbose)
+cmd.Parse(sys.argv)
+
+
+# helpers
+stack = ns.internet.InternetStackHelper()
+address = ns.internet.Ipv4AddressHelper()
+pointToPoint = ns.point_to_point.PointToPointHelper()
+
+
+# client star network
+pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue(f"{accessDelay.value}Mbps"))
+pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue(f"{accessRate.value}ms"))
+star = ns.point_to_point.PointToPointStarHelper(nClients.value, pointToPoint)
+star.InstallStack(stack)
+address.SetBase(ns.network.Ipv4Address("192.168.0.0"), ns.network.Ipv4Mask("255.255.255.0"))
+star.AssignIpv4Addresses(address)
+
+
+# point-to-point between R2 and Server
+serverNodes = ns.network.NodeContainer()
+serverNodes.Create(2)
+serverDevices = pointToPoint.Install(serverNodes)
+stack.Install(serverNodes)
+address.SetBase(ns.network.Ipv4Address("10.0.2.0"), ns.network.Ipv4Mask("255.255.255.0"))
+serverInterfaces = address.Assign(serverDevices)
+
+
+# point-to-point between R1 and R2
+routerNodes = ns.network.NodeContainer()
+routerNodes.Add(star.GetHub())
+routerNodes.Add(serverNodes.Get(0))
+pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue(f"{bottleneckRate.value}Mbps"))
+pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue(f"{bottleneckDelay.value}ms"))
+routerDevices = pointToPoint.Install(routerNodes)
+address.SetBase(ns.network.Ipv4Address("10.0.0.0"), ns.network.Ipv4Mask("255.255.255.252"))
+routerInterfaces = address.Assign(routerDevices)
+
