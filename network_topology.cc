6be33133a1979d06252310d7ea1c41d0caddba27
jdrp <jdiazderabago@gmail.com>
2024-04-16 13:12:14 +0200

    Added parameters

--- network_topology.cc
+++ network_topology.cc
@@ -24,17 +24,21 @@ NS_LOG_COMPONENT_DEFINE("MasticcTopology");
 int
 main(int argc, char * argv[]) {
     bool verbose = true;
-    uint32_t nClients = 3;
+    uint32_t nClients = 2;
     uint32_t accessRate = 1024; // Mbps
     uint32_t accessDelay = 5; // ms
     uint32_t bottleneckRate = 1; // Mbps
     uint32_t bottleneckDelay = 10; // ms
+    uint32_t meanNoiseInterval = 10; // ms
+    uint32_t meanNoiseSize = 1000; // bytes
     CommandLine cmd(__FILE__);
     cmd.AddValue("nClients", "Number of clients connected to R1", nClients);
     cmd.AddValue("accessRate", "Rate of access links (Mbps)", accessRate);
     cmd.AddValue("accessDelay", "Delay of access links (ms)", accessDelay);
     cmd.AddValue("bottleneckRate", "Rate of access links (Mbps)", bottleneckRate);
     cmd.AddValue("bottleneckDelay", "Delay of access links (ms)", bottleneckDelay);
+    cmd.AddValue("meanNoiseInterval", "Average delay between noise packets (ms)", meanNoiseInterval);
+    cmd.AddValue("meanNoiseSize", "Average size of noise packets (bytes)", meanNoiseInterval);
     cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
     cmd.Parse(argc, argv);
     if (verbose) {
@@ -44,14 +48,17 @@ main(int argc, char * argv[]) {
     nClients = nClients < 3 ? 3 : nClients;
     InternetStackHelper stack;
     Ipv4AddressHelper address;
+
     // client star network
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue(std::to_string(accessRate) + "Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue(std::to_string(accessDelay) + "ms"));
+    pointToPoint.SetQueue("ns3::DropTailQueue",  "MaxSize", QueueSizeValue (QueueSize ("1p")));
     PointToPointStarHelper star(nClients, pointToPoint);
     star.InstallStack(stack);
     address.SetBase("192.168.0.0", "255.255.255.0");
     star.AssignIpv4Addresses(address);
+
     // p2p between R2 and Server
     NodeContainer serverNodes;
     serverNodes.Create(2);
@@ -59,6 +66,7 @@ main(int argc, char * argv[]) {
     stack.Install(serverNodes);
     address.SetBase("10.0.2.0", "255.255.255.0");
     Ipv4InterfaceContainer serverInterfaces = address.Assign(serverDevices);
+
     // p2p between R1 and R2
     NodeContainer routerNodes;
     routerNodes.Add(star.GetHub());
@@ -68,6 +76,9 @@ main(int argc, char * argv[]) {
     NetDeviceContainer routerDevices = pointToPoint.Install(routerNodes);
     address.SetBase("10.0.0.0", "255.255.255.252");
     Ipv4InterfaceContainer routerInterfaces = address.Assign(routerDevices);
+    //Ptr<DropTailQueue> queue = DynamicCast<DropTailQueue>(DynamicCast<PointToPointNetDevice>(routerDevices.Get(0))->GetQueue());
+    //queue->SetAttribute ("MaxPackets", UintegerValue (3));
+
     // set up server
     UdpEchoServerHelper echoServer(9);
     ApplicationContainer serverApps = echoServer.Install(serverNodes.Get(1));
@@ -75,12 +86,15 @@ main(int argc, char * argv[]) {
     serverApps.Stop(Seconds(12.0));
     // set up noise client
     RandomNoiseClientHelper noiseClient(serverInterfaces.GetAddress(1), 9);
-    noiseClient.SetAttribute("IntervalMean", DoubleValue(0.01));
-    noiseClient.SetAttribute("PacketSizeMean", DoubleValue(1000.0));
-    noiseClient.SetAttribute("PacketSizeVariance", DoubleValue(1024.0));
+    noiseClient.SetAttribute("IntervalMean", DoubleValue(meanNoiseInterval / 1000.));
+    noiseClient.SetAttribute("PacketSizeMean", DoubleValue(meanNoiseSize));
+    float stdev = meanNoiseSize * .3;
+    float variance = stdev * stdev;
+    noiseClient.SetAttribute("PacketSizeVariance", DoubleValue(variance));
     ApplicationContainer noiseApps = noiseClient.Install(star.GetSpokeNode(0));
-    //noiseApps.Start(Seconds(0.5));
-    //noiseApps.Stop(Seconds(10.0));
+    noiseApps.Start(Seconds(2.0));
+    noiseApps.Stop(Seconds(10.0));
+
     // set up main client
     UdpEchoClientHelper echoClient(serverInterfaces.GetAddress(1), 9);
     echoClient.SetAttribute("MaxPackets", UintegerValue(0));
@@ -88,7 +102,7 @@ main(int argc, char * argv[]) {
     echoClient.SetAttribute("PacketSize", UintegerValue(1024));
     ApplicationContainer clientApps = echoClient.Install(star.GetSpokeNode(1));
     clientApps.Start(Seconds(1.0));
-    clientApps.Stop(Seconds(9.0));
+    clientApps.Stop(Seconds(12.0));
 
     /*Ptr < UniformRandomVariable > interval_randomizer = CreateObject < UniformRandomVariable > ();
     interval_randomizer -> SetAttribute("Min", DoubleValue(0.005));
